f0ce4c2c08eefa38eaa6c96f760741ff075f9f5a
DT21DT <145446095+DT21DT@users.noreply.github.com>
2024-02-15 18:13:07 +0000

    sistemazione anche stile

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     const bins = document.querySelectorAll(".bin");
     const items = document.querySelectorAll(".item");
+
+    items.forEach(item => {
+        const height = parseInt(item.style.getPropertyValue("--height"));
+        item.classList.add('height-' + height);
+    });
     let totalCost = 0;
     let binsUsed = {}; // Oggetto per tenere traccia dei bin utilizzati e del numero di item al loro interno
+    let initialPosition = {}; // Oggetto per memorizzare la posizione iniziale degli item
 
     function handleDragStart(event) {
         event.dataTransfer.setData("text/plain", event.target.id);
@@ -11,6 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
     items.forEach(item => {
         item.setAttribute("draggable", "true");
         item.addEventListener("dragstart", handleDragStart);
+        initialPosition[item.id] = item.parentNode; // Memorizza la posizione iniziale dell'item
+
     
         // Aggiungiamo un event listener per il click
         item.addEventListener("click", function(event) {
@@ -27,10 +35,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 if (binsUsed[bin.id] === 0) {
                     const cost = parseInt(bin.dataset.cost);
                     totalCost -= cost;
-                    document.getElementById("totalCost").innerText = "Costo totale: $" + totalCost.toFixed(2);
+                    document.getElementById("totalCost").innerText = "Costo totale: £" + totalCost.toFixed(2);
                 }
+                
+                console.log("Numero di item associati ai bin:", binsUsed);
+                // Riposiziona l'elemento al suo genitore iniziale
+                const initialParent = initialPosition[item.id];
+                initialParent.appendChild(item);
+                sortItemsByHeight(); // Riordina gli item per altezza
             }
-            console.log("Numero di item associati ai bin:", binsUsed);
         });
     });
     
@@ -69,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
                     const cost = parseInt(bin.dataset.cost);
                     totalCost += cost;
                     binsUsed[bin.id] = 1;
-                    document.getElementById("totalCost").innerText = "Costo totale: $" + totalCost.toFixed(2);
+                    document.getElementById("totalCost").innerText = "Costo totale: £" + totalCost.toFixed(2);
                 } else {
                     // Aggiorna il numero di item nel bin
                     binsUsed[bin.id]++;
@@ -96,4 +109,21 @@ document.addEventListener("DOMContentLoaded", function() {
     bins.forEach(bin => {
         bin.setAttribute("data-item-id", bin.id);
     });
+
+    // Funzione per riordinare gli item in base all'altezza (ordine decrescente) e all'ID (ordine crescente)
+    function sortItemsByHeight() {
+        const container = document.querySelector('.container-items');
+        const itemsArray = Array.from(container.children);
+        itemsArray.sort((a, b) => {
+            const heightA = parseInt(a.style.getPropertyValue("--height"));
+            const heightB = parseInt(b.style.getPropertyValue("--height"));
+            if (heightA === heightB) {
+                return parseInt(a.id.slice(4)) - parseInt(b.id.slice(4)); // Ordine crescente per ID
+            }
+            return heightB - heightA; // Ordine decrescente per altezza
+        });
+        container.innerHTML = "";
+        itemsArray.forEach(item => container.appendChild(item));
+    }
+
 });
